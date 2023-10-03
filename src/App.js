5c6afffce6f6c2c073d2d9f10b273e7b3815c733
FERNANDES Paul <paulfernandes266@gmail.com>
2023-10-03 18:21:06 +0200

    wip maj4

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 
-import logo from './logo.svg';
-import './App.css';
-import data from "./data/characters.json"
+import {NumberofCharacters} from './components/NumberofCharacters '
+import {ListeCharacters} from './components/ListeCharacters '
+import {Header} from './components/Header ';
+
+
 const characters = require("./data/characters.json");
 
-function Title({ color = 'red', children, hidden = false, ...props }) {
- if (hidden) {
- return null;
- }
-
- return (<h1 style={{color: color}} {...props}>{children}</h1>);
-}
-
-function ListeCharacters(){
- return(<ul>
- {data.map((item, index) => (
- <li key={index}>{item.name}</li> 
- ))}
- </ul>);
- }
-
 function App() {
+
 return (
- <>
- {/* 
- La propriété id et data-demo sont passées au composant Title grâce au spread operator.
- La propriété color est définie dans le composant Title
- */}
- <Title color="black" id="my-id" data-demo="demo">Liste de personnage</Title>
- <ListeCharacters></ListeCharacters>
+  <>
+
+ <Header/>
+ <NumberofCharacters characters={characters}/>
+ <br />
+ <ListeCharacters characters ={characters}/>
  </>
 );
 }
