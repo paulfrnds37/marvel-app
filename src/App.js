9e405df87c599ef5f1210b27d2d938caa2252bb2
FERNANDES Paul <paulfernandes266@gmail.com>
2023-10-03 17:53:36 +0200

    maj2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+
 import logo from './logo.svg';
 import './App.css';
+import data from "./characters.json"
+const characters = require("./characters.json");
+
+function Title({ color = 'red', children, hidden = false, ...props }) {
+ if (hidden) {
+ return null;
+ }
+
+ return (<h1 style={{color: color}} {...props}>{children}</h1>);
+}
+
+function ListeCharacters(){
+ return(<ul>
+ {data.map((item, index) => (
+ <li key={index}>{item.name}</li> 
+ ))}
+ </ul>);
+ }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+ <>
+ {/* 
+ La propriété id et data-demo sont passées au composant Title grâce au spread operator.
+ La propriété color est définie dans le composant Title
+ */}
+ <Title color="black" id="my-id" data-demo="demo">Liste de personnage</Title>
+ <ListeCharacters></ListeCharacters>
+ </>
+);
 }
 
 export default App;
