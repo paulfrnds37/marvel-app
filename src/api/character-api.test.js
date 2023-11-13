b2fbbd8f854db29bf4824b8bb508b6c83f0aac29
FERNANDES Paul <paulfernandes266@gmail.com>
2023-11-13 17:32:42 +0100

    Add unit tests for character-api

--- src/api/character-api.test.js
+++ src/api/character-api.test.js
@@ -17,4 +17,51 @@ describe('character-api', () => {
     });
 
   });
+
+  describe('getCharacterById', () => {
+    test('returns the character with the given id when id is a string', () => {
+      // when
+      const id = "1009663";
+      const name = "Thor";
+
+      // then
+      const character = getCharacterById(id);
+
+      // expect
+      expect(character.id).toBe(id);
+      expect(character.name).toBe(name);
+    });
+
+    test('returns the character with the given id when id is a number', () => {
+      // when
+      const id = 1009663;
+      const name = "Thor";
+
+      // then
+      const character = getCharacterById(id);
+
+      // expect
+      expect(character.id).toBe(id.toString());
+      expect(character.name).toBe(name);
+    });
+
+    test('throws an error if id is not provided', () => {
+      expect(() => {
+        getCharacterById();
+      }).toThrow(`Parameter id must be a number or a string, but it was undefined`);
+    });
+
+    test('throws an error if id is not a number or a string', () => {
+      expect(() => {
+        getCharacterById({});
+      }).toThrow(`Parameter id must be a number or a string, but it was object`);
+    });
+
+    test('throws an error if character with given id is not found', () => {
+      const id = 999;
+      expect(() => {
+        getCharacterById(id);
+      }).toThrow(`Character with id ${id} not found`);
+    });
+  });
 });
